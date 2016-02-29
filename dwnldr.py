ee3131463efabb7a64796d94a1fee1a80ccd81f7
Raju Kadam <rajulkadam@gmail.com>
2016-02-29 12:28:24 -0800

    Make code PEP8 compliant

--- dwnldr.py
+++ dwnldr.py
@@ -1,21 +1,23 @@
-import urllib3
-import multiprocessing
-import shutil
 import glob
+import multiprocessing
 import os
+import shutil
 import sys
 
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import as_completed
+import urllib3
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 TEN_MB = 10 * 1024 * 1024
-HUNDRED_MB = 10 * TEN_MB 
+HUNDRED_MB = 10 * TEN_MB
 pm = urllib3.PoolManager()
+
 
 def get_content_size(url):
     with pm.urlopen('GET', url, preload_content=False) as url_fd:
         size = url_fd.getheader('Content-Length')
     return int(size)
+
 
 def combine_parts(url):
     fname = url.split('/')[-1]
@@ -23,24 +25,25 @@ def combine_parts(url):
 
     # Get list of all file parts
     fparts = glob.glob('fpart*')
-    
+
     if not fparts:
         print 'something went terribly wrong'
         return
 
     # Need to sort inplace, does not return anything
     fparts.sort()
-    
+
     with open(fname, 'wb') as finalFname:
         for fpart in fparts:
             with open(fpart, 'rb') as tempFname:
                 shutil.copyfileobj(tempFname, finalFname, TEN_MB)
                 print 'copying {0}'.format(fpart)
-    
+
     for fpart in fparts:
         os.remove(fpart)
 
     print 'done merging'
+
 
 def download_worker(url, file_start, file_end, fpart_name):
     # XXX check if url supports range
@@ -48,7 +51,8 @@ def download_worker(url, file_start, file_end, fpart_name):
 
     chunk_size = file_end - file_start
     headers = {'Range': 'bytes={0}-{1}'.format(file_start, file_end)}
-    with pm.urlopen('GET', url, headers=headers, preload_content=False) as url_fd:
+    with pm.urlopen('GET', url, headers=headers,
+                    preload_content=False) as url_fd:
         with open(fpart_name, 'wb') as fp:
             while chunk_size > HUNDRED_MB:
                 fp.write(url_fd.read(HUNDRED_MB))
@@ -59,23 +63,26 @@ def download_worker(url, file_start, file_end, fpart_name):
     print '{0} done'.format(fpart_name)
     return True
 
+
 def download_url(url):
     cpu_count = multiprocessing.cpu_count()
     remote_file_sz = get_content_size(url)
-    chunk_size = remote_file_sz/cpu_count;
+    chunk_size = remote_file_sz/cpu_count
     start = 0
     end = chunk_size
 
-    print 'Downloading {0} {1} bytes'.format(url.split('/')[-1], remote_file_sz)
+    print 'Downloading {0} {1} bytes'.format(
+        url.split('/')[-1], remote_file_sz)
 
     with ThreadPoolExecutor(max_workers=cpu_count) as executor:
         futures_to_parts = {}
-   
+
         for idx in range(cpu_count):
             if end > remote_file_sz:
                 end = remote_file_sz
             fpart_name = 'fpart{0}'.format(idx)
-            futures_to_parts[executor.submit(download_worker, url, start, end, fpart_name)] = fpart_name
+            futures_to_parts[executor.submit(download_worker, url, start,
+                                             end, fpart_name)] = fpart_name
             start = end + 1
             end += chunk_size
 
@@ -86,7 +93,8 @@ def download_url(url):
             except Exception as e:
                 print e
     executor.shutdown(wait=True)
- 
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print 'usage: dwnldr.py <url>'
